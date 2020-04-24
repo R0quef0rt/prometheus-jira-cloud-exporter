4fa9b274fad2a1374ad01d6e780de81c6389b099
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-23 21:00:46 -0500

    Started working with lists for project name

--- jiraFunctions.py
+++ jiraFunctions.py
@@ -15,19 +15,21 @@ class Issues:
             jql = ''
             block_size = 100
             block_num = 0
-            result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size, 
-                                        fields="issuetype, created, resolutiondate, reporter, assignee, status")
+            totalIssues = 0
+            projectLabels = []
+            result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size,
+                                        fields="project, issuetype, created, resolutiondate, reporter, assignee, status")
 
             while bool(result):
                 for issue in result:
-                    issue_key = issue.key
-                    print(issue_key)
-                    # issue.fields.status.name
-
-                block_num = block_num + 1
+                    totalIssues += 1
+                    print(issue.fields.project)
+                    # projectLabels = list(set(issue.fields.project))
+                    # print(projectLabels)
+                block_num += 1
                 time.sleep(5)
-                result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size, 
-                                        fields="issuetype, created, resolutiondate, reporter, assignee, status")
+                result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size,
+                                        fields="project, issuetype, created, resolutiondate, reporter, assignee, status")
             jira.close()
         except (AttributeError):
             jira.close()
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-        jql = ''
-        block_size = 2
-        block_num = 0
-        header = True
-        result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size, 
-                                    fields="issuetype, created, resolutiondate, reporter, assignee, status")
-        print(bool(result))
