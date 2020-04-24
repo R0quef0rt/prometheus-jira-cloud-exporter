2d00368eda61f8d88a2a2306cefee14d9a09cfc0
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-24 07:31:49 -0500

    Add projectLabels to a list

--- jiraFunctions.py
+++ jiraFunctions.py
@@ -13,9 +13,10 @@ class Issues:
         
         try:
             jql = ''
-            block_size = 100
+            block_size = 2
             block_num = 0
             totalIssues = 0
+            global projectLabels
             projectLabels = []
             result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size,
                                         fields="project, issuetype, created, resolutiondate, reporter, assignee, status")
@@ -23,17 +24,22 @@ class Issues:
             while bool(result):
                 for issue in result:
                     totalIssues += 1
-                    print(issue.fields.project)
-                    # projectLabels = list(set(issue.fields.project))
-                    # print(projectLabels)
+                    projectLabels.append(str(issue.fields.project))
+
                 block_num += 1
                 time.sleep(5)
                 result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size,
                                         fields="project, issuetype, created, resolutiondate, reporter, assignee, status")
+
+            # Convert list to a set, then back to a list, in order to obtain unique values                            
+            projectLabels = list(set(projectLabels))
             jira.close()
+
         except (AttributeError):
+
             jira.close()
 
 
 
 Issues.totalIssues()
+print(projectLabels)
