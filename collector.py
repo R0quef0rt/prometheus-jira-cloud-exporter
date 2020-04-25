3a00c6fffec8dfefd6240c43551dc5afbd90d463
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-25 01:49:29 -0500

    Used tutorial

--- collector.py
+++ collector.py
@@ -1,24 +1,86 @@
-import time
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
 from prometheus_client import start_http_server
+from jira import JIRA
+from variables import *
+from secrets import *
+import globals
+from globals import *
+import time
+
+class Issue:
+
+    @staticmethod
+    def search():
+
+        try:
+            
+            jql = ''
+            block_size = 100
+            block_num = 0
+            count = 0
+            global promLabels
+            promLabels = []
+            result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size,
+                                        fields="project, summary, components, labels, status, issuetype, resolution, created, resolutiondate, reporter, assignee, status")
+
+            while bool(result):
+
+                for issue in result:
+                    count += 1
+                    project = str(issue.fields.project)
+                    assignee = str(issue.fields.assignee)
+                    issueType = str(issue.fields.assignee)
+                    status = str(issue.fields.status)
+                    resolution = str(issue.fields.resolution)
+                    reporter = str(issue.fields.reporter)
+                    components = issue.fields.components
+                    labels = issue.fields.labels
+                    
+                    # Construct the PromQL query
+                    promLabel = [f'{project}', f'{assignee}', f'{issueType}', f'{status}', f'{resolution}', f'{reporter}']
+                    
+                    if components:
+                        for component in components:
+                            promLabel.append(f'{component}')
+                    else:
+                        promLabel.append(f'None')
+
+                    if labels:
+                        for label in labels:
+                            promLabel.append(f'{label}')
+                    else:
+                        promLabel.append(f'None')
+                
+                    promLabels.append(promLabel)
+
+                block_num += 1
+                time.sleep(5)
+                result = jira.search_issues(jql, startAt=block_num*block_size, maxResults=block_size,
+                                        fields="project, summary, components, labels, status, issuetype, resolution, created, resolutiondate, reporter, assignee, status")
+            jira.close()
+
+            return promLabels
+
+        except (AttributeError):
+
+            jira.close()
 
 
-class CustomCollector(object):
+class IssueCollector(object):
     def __init__(self):
         pass
 
     def collect(self):
-        g = GaugeMetricFamily("MemoryUsage", 'Help text', labels=['instance'])
-        g.add_metric(["instance01.us.west.local"], 20)
-        yield g
-
-        c = CounterMetricFamily("HttpRequests", 'Help text', labels=['app'])
-        c.add_metric(["example"], 2000)
-        yield c
+        issuesGauge = GaugeMetricFamily('jira_issues', 'Jira issues', labels=['project', 'assignee', 'issueType', 'status', 'resolution', 'reporter', 'component', 'label'])
+        Issue.search()
+        for labels in promLabels:
+            print(labels)
+            issuesGauge.add_metric(labels, 20)
+            yield issuesGauge
 
 
 if __name__ == '__main__':
     start_http_server(8000)
-    REGISTRY.register(CustomCollector())
+    REGISTRY.register(IssueCollector())
     while True:
         time.sleep(1)
