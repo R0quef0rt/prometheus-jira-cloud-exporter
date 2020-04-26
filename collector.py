b0deb918d99a38d6be6811ba54f2cc595d8d3a9e
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-26 08:42:36 -0500

    Ran code through black python formatter

--- collector.py
+++ collector.py
@@ -4,16 +4,11 @@ from jira import JIRA, JIRAError
 from config import *
 import time
 
-jira = JIRA(
-    basic_auth=(user, apikey), 
-    options={
-        'server': instance
-    }
-)
+jira = JIRA(basic_auth=(user, apikey), options={"server": instance})
 
 
 class IssueCollector:
-    
+
     block_num = 0
     promOutput = {}
 
@@ -22,16 +17,20 @@ class IssueCollector:
 
         # Search Jira API
         block_size = 100
-        result = jira.search_issues(jql, startAt=self.block_num*block_size, maxResults=block_size,
-                                    fields="project, summary, components, labels, status, issuetype, resolution, created, resolutiondate, reporter, assignee, status") 
+        result = jira.search_issues(
+            jql,
+            startAt=self.block_num * block_size,
+            maxResults=block_size,
+            fields="project, summary, components, labels, status, issuetype, resolution, created, resolutiondate, reporter, assignee, status",
+        )
 
-        return result       
+        return result
 
     @classmethod
     def construct(self, jql):
 
         try:
-            
+
             promLabels = []
             result = IssueCollector.search(jql)
 
@@ -49,54 +48,68 @@ class IssueCollector:
                     reporter = str(issue.fields.reporter)
                     components = issue.fields.components
                     labels = issue.fields.labels
-                    
+
                     # Construct the list of labels from attributes
-                    promLabel = [project, assignee, issueType, status, resolution, reporter]
-                    
+                    promLabel = [
+                        project,
+                        assignee,
+                        issueType,
+                        status,
+                        resolution,
+                        reporter,
+                    ]
+
                     if components:
                         for component in components:
                             promLabel.append(str(component))
                     else:
-                        promLabel.append('None')
-
+                        promLabel.append("None")
                     if labels:
                         for label in labels:
                             promLabel.append(str(label))
                     else:
-                        promLabel.append('None')
-                
+                        promLabel.append("None")
                     promLabels.append(promLabel)
-
                 # Increment the results via pagination
                 self.block_num += 1
                 time.sleep(2)
                 result = IssueCollector.search(jql)
-
             jira.close()
-            
+
             # Convert nested lists into a list of tuples, so that we may hash and count duplicates
             for l in promLabels:
                 self.promOutput.setdefault(tuple(l), list()).append(1)
             for k, v in self.promOutput.items():
                 self.promOutput[k] = sum(v)
-            
             return self.promOutput
-
         except (JIRAError, AttributeError):
 
             jira.close()
 
     def collect(self):
-        
+
         # Set up the Issues Prometheus gauge
-        issuesGauge = GaugeMetricFamily('jira_issues', 'Jira issues', labels=['project', 'assignee', 'issueType', 'status', 'resolution', 'reporter', 'component', 'label'])
+        issuesGauge = GaugeMetricFamily(
+            "jira_issues",
+            "Jira issues",
+            labels=[
+                "project",
+                "assignee",
+                "issueType",
+                "status",
+                "resolution",
+                "reporter",
+                "component",
+                "label",
+            ],
+        )
 
         for labels, value in self.promOutput.items():
             issuesGauge.add_metric(labels, value)
         yield issuesGauge
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Start the webserver, search Jira, and register the issue collector
     start_http_server(8000)
