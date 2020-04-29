5babd9c6a1a61e997145061ea2520751e460d9e2
R0quef0rt <ryanjbrooks11@gmail.com>
2020-04-29 00:27:11 -0500

    Completed test coverage

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,9 +4,9 @@ from classes.core import IssueCollector
 
 class TestIssueCollector(unittest.TestCase):
 
-    def test_search(self):
-        result = IssueCollector.search('project=TEST')
-        self.assertIn('TEST', str(result))
+    def test_construct(self):
+        out = IssueCollector.construct('project=TEST')
+        self.assertIn('TEST', str(out))
 
 
 if __name__ == '__main__':
